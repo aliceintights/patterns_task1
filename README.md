6e80b6f33448dc4f1a4c21a19f03317474ac72c0
irina <obesyuk@gmail.com>
2024-06-16 17:16:35 +0300

    allure added

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 [![Java CI with Gradle](https://github.com/aliceintights/patterns_task1/actions/workflows/gradle.yml/badge.svg)](https://github.com/aliceintights/patterns_task1/actions/workflows/gradle.yml)
+
+![img.png](img.png)
+![img_1.png](img_1.png)
--- src/test/java/ru/netology/web/CardDeliveryDateChangeTest/CardDeliveryDateChange.java
+++ src/test/java/ru/netology/web/CardDeliveryDateChangeTest/CardDeliveryDateChange.java
@@ -1,5 +1,10 @@
 package ru.netology.web.CardDeliveryDateChangeTest;
 
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import ru.netology.web.DataGenerator.CardReceiverDataGenerator;
@@ -9,8 +14,18 @@ import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
-
 public class CardDeliveryDateChange {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
     @Test
     void changeDateForCardDelivery() {
         open("http://localhost:9999");
